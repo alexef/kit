101699984c71579b9207944535a1f4b06a122ed1
Alex Eftimie <alex@eftimie.ro>
2012-02-19 13:32:24 +0200

    Fix broken admin links.

--- kit/urls.py
+++ kit/urls.py
@@ -11,13 +11,17 @@ urlpatterns = patterns('',
     # url(r'^kit/', include('kit.foo.urls')),
 
     url(r'^$', HomePage.as_view(), name='projects_list'),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^user/login/', 'django.contrib.auth.views.login'),
+    url(r'^user/logout/', 'django.contrib.auth.views.logout', dict(next_page='/')),
+
+    # these would break the previous regs
     url(r'^(?P<project>.+)/issues/', include('kit.issues.urls')),
     url(r'^(?P<project>.+)/activity/', include('kit.activity.urls')),
     url(r'^(?P<project>.+)/manage/$', ManageProject.as_view(), name='project_manage'),
     url(r'^(?P<project>.+)/manage/add/$', PUCreate.as_view(), name='project_manage_add'),
     url(r'^(?P<project>.+)/manage/edit/(?P<pk>\d+)/$', PUUpdate.as_view(), name='project_manage_edit'),
 
-    url(r'^user/login/', 'django.contrib.auth.views.login'),
-    url(r'^user/logout/', 'django.contrib.auth.views.logout', dict(next_page='/')),
-    url(r'^admin/', include(admin.site.urls)),
+
 )

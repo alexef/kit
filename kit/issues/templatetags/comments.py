ba68b3f05fde8f6d716927121f39fb236e00b7b8
Alex Eftimie <alex@eftimie.ro>
2012-02-19 10:53:16 +0200

    Fix #17, prettify comments, add gravatars.

--- kit/issues/templatetags/comments.py
+++ kit/issues/templatetags/comments.py
@@ -1,3 +1,4 @@
+import hashlib
 from django.template.loader import render_to_string
 from django import template
 register = template.Library()
@@ -5,3 +6,9 @@ register = template.Library()
 @register.simple_tag
 def do_comment(comm):
     return render_to_string('issues/comment.html', dict(c=comm))
+
+@register.simple_tag
+def gravatar(author):
+    md5 = hashlib.md5(author.email).hexdigest()
+
+    return 'http://www.gravatar.com/avatar/%s' % md5

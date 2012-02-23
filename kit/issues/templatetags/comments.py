461e84dd2a5bcb45087073aeaf435cc7289e8e48
Alex Eftimie <alex@eftimie.ro>
2012-02-23 09:07:44 +0200

    Fix #61: linkify urls and hash tag issue references

--- kit/issues/templatetags/comments.py
+++ kit/issues/templatetags/comments.py
@@ -1,3 +1,4 @@
+import re
 import hashlib
 from django.template.loader import render_to_string
 from django import template
@@ -5,10 +6,19 @@ register = template.Library()
 
 @register.simple_tag
 def do_comment(comm):
-    return render_to_string('issues/comment.html', dict(c=comm))
+    return render_to_string('issues/comment.html', dict(c=comm, project=comm.issue.project))
 
 @register.simple_tag
 def gravatar(author):
     md5 = hashlib.md5(author.email).hexdigest()
 
     return 'http://www.gravatar.com/avatar/%s' % md5
+
+@register.filter
+def linkify(text, project=None):
+    print project
+    pat1 = re.compile(r"(^|[\n ])(\#(\d+))", re.DOTALL)
+    text = pat1.sub(r'\1<a href="/%s/issues/view/\3">\2</a>' % project, text)
+    pat = re.compile(r"(^|[\n ])(([\w]+?://[\w\#$%&~.\-;:=,?@\[\]+]*)(/[\w\#$%&~/.\-;:=,?@\[\]+]*)?)", re.IGNORECASE | re.DOTALL)
+    text = pat.sub(r'\1<a href="\2" target="_blank">\3</a>', text)
+    return text
